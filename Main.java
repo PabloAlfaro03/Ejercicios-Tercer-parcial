69c56d33d23b682193edbad7ff4459b1a742655d
PabloAlfaro03 <122295148+PabloAlfaro03@users.noreply.github.com>
2023-03-28 21:29:07 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
-package superClase;
 
-
+import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
-            Game game=new Game();
-            game.gameStart();
-        }//main
+
+    Manager manager = new Manager();
+    try {
+
+        manager.correrPrograma();
+
+        System.out.println("Archivos generados");
+    }catch (IOException e){
+        System.out.println("Error intente de nuevo" + e.getMessage());
+
     }
+    }
+}
