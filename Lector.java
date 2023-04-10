66f41bde1c394bfbf4811e85888340e08704bbde
PabloAlfaro03 <122295148+PabloAlfaro03@users.noreply.github.com>
2023-04-09 23:45:15 -0600

    Add files via upload

--- Lector.java
+++ Lector.java
@@ -1,78 +1,73 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.MessageFormat;
 import java.util.List;
-import java.io.FileWriter;
-import java.io.IOException;
 import static java.lang.Integer.parseInt;
+
 public class Lector {
 
+    private static final String Archivo = "emails.csv";
+    private static final String ArchivoFinal = "173549.txt";
+    private static final int Correos_A_Leer = 51;
 
+    //#########################################################################################
     public void ejecutar(int id) {
 
         int alfaroID = parseInt(String.valueOf(id).substring(String.valueOf(id).length() - 3));
-        System.out.println("El rango de 50 empezara a partir del siguiente id:" + alfaroID);
+        System.out.println("se analizaran 50 correos a partir del siguiente id: " + alfaroID);
 
-        List<String> lineas = null;
-        //###################################################################
-        String nombreArchivo = "emails.csv";
-        Path currentWorkingDir = Paths.get(System.getProperty("user.dir"));
+        List<String> lines = readFile();
+        if (lines == null) {
+            throw new RuntimeException("Archivo no encontrado");
+        }
+
+        int limite = alfaroID + Correos_A_Leer;
+
+        System.out.println("ya se analizaron del " + alfaroID + " al " + (limite - 1));
+
+        int[] sums = new int[lines.get(0).split(",").length - 1];
+
+        for (int i = alfaroID; i < limite; i++) {
+            String[] values = lines.get(i).split(",");
+
+            for (int j = 1; j < values.length; j++) {
+                sums[j - 1] += parseInt(values[j]);
+            }
+        }
+
+        writeToFile(sums, lines.get(0).split(","));
+    }
+
+    //########################################################################################
+    private List<String> readFile() {
         try {
-
-            Path PathdeArchivo;
-
-            PathdeArchivo = Files.walk(currentWorkingDir)
-                    .filter(path -> path.getFileName().toString().equals(nombreArchivo))
+            Path filePath = Files.walk(Paths.get(System.getProperty("user.dir")))
+                    .filter(path -> path.getFileName().toString().equals(Lector.Archivo))
                     .findFirst()
-                    .orElseThrow(() -> new RuntimeException("Archivo no encontrado"));
-            lineas = Files.readAllLines(PathdeArchivo);
+                    .orElse(null);
 
+            return (filePath != null) ? Files.readAllLines(filePath) : null;
         } catch (IOException e) {
-            
             e.printStackTrace();
+            return null;
         }
-        //###################################################################
+    }
 
-
-        int fin = alfaroID + 50;
-        System.out.println("ya se analizaron del 549 al 599 " + fin);
-
-        //###################################################################
-
-        int[] sumas = new int[3000];
-
-        for (int i = alfaroID; i <= fin; i++) {
-            String[] correo = new String[0];
-            if (lineas != null) {
-                correo = lineas.get(i).split(",");
-            }
-
-            for (int j = 1; j < correo.length - 1; j++) {
-                sumas[j - 1] += parseInt(correo[j]);
-            }
-        }
-
-        String[] formato = new String[0];
-        if (lineas != null) {
-            formato = lineas.get(0).split(",");
-        }
-        //###################################################################
-
+    //###############################################################################################
+    private void writeToFile(int[] sums, String[] header) {
         try {
-            FileWriter escribir = new FileWriter("C:/Users/LENOVO/IntelliJ IDEA Community Edition 2022.3.2/Parcial3/src/173549.txt");
-            for (int i = 0; i < sumas.length; i++) {
-                escribir.write(formato[i + 1] + ", ");
-                escribir.write(MessageFormat.format("{0}\n", sumas[i]));
+            FileWriter writer = new FileWriter(ArchivoFinal);
+            for (int i = 0; i < sums.length; i++) {
+                writer.write(header[i + 1] + ", ");
+                writer.write(MessageFormat.format("{0}\n", sums[i]));
             }
-            escribir.close();
-        }catch (IOException e){
+            writer.close();
+        } catch (IOException e) {
             e.printStackTrace();
+        }
     }
-    }
-
-
-
-
 
 }
